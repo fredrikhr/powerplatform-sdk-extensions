4981ab673817b2a686cc768959cd517bbb466443
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2025-05-04 15:10:37 +0200

    Add RetrieveOAuthChallengeParameters custom API

--- pluginpackages/FredrikHr.PowerPlatformSdkExtensions.DataverseInformationPlugins/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage(
-    "Naming",
-    "CA1707: Identifiers should not contain underscores",
-    Scope = "module",
-    Justification = nameof(IPlugin)
-)]
