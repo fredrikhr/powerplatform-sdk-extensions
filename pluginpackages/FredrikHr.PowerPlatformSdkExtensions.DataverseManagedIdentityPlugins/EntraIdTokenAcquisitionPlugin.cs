d29f87548a593f851eca3001e2155519f6c6c231
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2025-05-08 20:56:19 +0200

    Add Plugin for ITokenService.RetrieveAADAccessToken

---
+++ pluginpackages/FredrikHr.PowerPlatformSdkExtensions.DataverseManagedIdentityPlugins/EntraIdTokenAcquisitionPlugin.cs
@@ -0,0 +1,35 @@
+namespace FredrikHr.PowerPlatformSdkExtensions.DataverseManagedIdentityPlugins;
+
+public class EntraIdTokenAcquisitionPlugin : IPlugin
+{
+    public void Execute(IServiceProvider serviceProvider)
+    {
+        var context = serviceProvider.Get<IPluginExecutionContext6>();
+        var outputs = context.OutputParameters;
+
+        var scopes = context.InputParameterOrDefault<string[]>("Scopes");
+
+        var tokenAcquirer = serviceProvider.Get<ITokenService>();
+        try
+        {
+            string accessToken = tokenAcquirer.RetrieveAADAccessToken(
+                context.InputParameterOrDefault<string>("ClientId"),
+                context.InputParameterOrDefault<string>("ResourceId"),
+                context.InputParameterOrDefault<string?>("TenantId")
+                switch
+                {
+                    { Length: > 0 } tenantId => tenantId,
+                    _ => context.TenantId.ToString()
+                }
+            );
+            outputs["AccessToken"] = accessToken;
+        }
+        catch (Exception except)
+        {
+            throw new InvalidPluginExecutionException(
+                message: except.Message,
+                exception: except
+                );
+        }
+    }
+}
